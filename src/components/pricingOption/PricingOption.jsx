5025c84d14112196765c99343d75b905e2972a29
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-04-15 01:32:11 +0600

    added daisy card

--- src/components/pricingOption/PricingOption.jsx
+++ src/components/pricingOption/PricingOption.jsx
@@ -1,5 +1,6 @@
 import React, { use } from "react";
 import PricingCard from "../pricingCard/pricingCard";
+import DaisyCard from "../DaisyCard/DaisyCard";
 const PricingOption = ({ pricingPromise }) => {
   const pricingData = use(pricingPromise);
   //   console.log(pricingData);
@@ -8,8 +9,12 @@ const PricingOption = ({ pricingPromise }) => {
     <div className="p-4">
       <h2 className="text-5xl">Get Our Membership</h2>
       <div className="grid md:grid-cols-3 gap-8">
-        {pricingData.map((pricing) => (
+        {/* {pricingData.map((pricing) => (
           <PricingCard key={pricing.id} pricing={pricing}></PricingCard>
+        ))} */}
+
+        {pricingData.map((pricing) => (
+          <DaisyCard key={pricing.id} pricing={pricing}></DaisyCard>
         ))}
       </div>
     </div>
